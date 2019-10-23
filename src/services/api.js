cf743af924d97ed1593b566cb266b2fde22868c8
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2019-10-22 23:03:41 -0300

    fix: andress of api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://amqpgz2wi7.execute-api.us-east-1.amazonaws.com/dev/',
+  baseURL: 'https://amqpgz2wi7.execute-api.us-east-1.amazonaws.com/dev',
 });
 
 export default api;
