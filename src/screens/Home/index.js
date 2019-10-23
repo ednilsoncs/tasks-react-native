76be6201c9b7879eda22ba4a473179956112a1d1
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2019-10-22 23:04:09 -0300

    feat: render new screen for show tasks

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import { Title } from '../../components';
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+import {
+  View, FlatList, Text, TouchableOpacity,
+} from 'react-native';
+import { Title, Subtitle, Card } from '../../components';
 import { Container } from './styles';
+import { api } from '../../services';
 
-export default function Home() {
+function Home() {
+  const [tasks, setTasks] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
+  const handleGetTasks = async () => {
+    setLoading(true);
+    try {
+      const { data } = await api.get('/');
+      setTasks(data);
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    handleGetTasks();
+  }, []);
+  const renderItem = ({ item }) => (
+    <TouchableOpacity>
+      <View>
+        <Card>
+          <Title>{item.name}</Title>
+          <Subtitle>{item.description}</Subtitle>
+          <Text>{item.made === 1 ? 'feito' : 'Não feito' }</Text>
+        </Card>
+      </View>
+    </TouchableOpacity>
+
+  );
   return (
     <Container>
+      {// console.log(tasks)
+      }
       <Title>Suas Tarefas</Title>
+      { !isLoading && (
+      <FlatList
+        keyExtractor={(item) => item}
+        renderItem={renderItem}
+        data={tasks.return.rows}
+      />
+      )}
+
     </Container>
   );
 }
+export default Home;
