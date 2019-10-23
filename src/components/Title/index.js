7abe25e08fa3c00937ac29c0ae54121b4af6d554
ednilsonCS <ednilsoncs@dcomp.ufs.br>
2019-10-22 23:02:53 -0300

    feat: addead componente Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,6 +3,7 @@ import { colors } from '../../theme';
 
 const Title = styled.Text`
   color: ${colors.black};
+  font-size: 18;
   text-transform: ${(props) => (props.isUppercase ? 'upercase' : 'none')}
 `;
 export default Title;
